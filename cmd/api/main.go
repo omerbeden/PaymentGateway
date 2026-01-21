28ac4d8bbb8128dc30ce8d19a3ccb520790aab1b
omerbeden <omerbeden@gmail.com>
2026-01-21 13:48:29 +0300

    add gin

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/omerbeden/paymentgateway/internal/adapter/handler/http/routes"
+)
+
+func main() {
+
+	router := routes.SetupRoutes()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		log.Println("Starting server on :8080")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Listen error: %s", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %s", err)
+	}
+
+	log.Println("Server exited gracefully")
+
+}
